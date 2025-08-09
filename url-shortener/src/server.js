5028d62729ee9a99cb6f5f998fa9896a87892c2c
Sparrow8104 <anchalsingh8102004@gmail.com>
2025-08-10 00:09:52 +0530

    Fix base URL port in .env and update README

--- url-shortener/src/server.js
+++ url-shortener/src/server.js
@@ -2,24 +2,29 @@ const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const connectDB = require('./config/index');
+const urlRoutes = require('./routes/urlRoutes');
 
 dotenv.config();
+connectDB();
+
 const app = express();
 
 
 app.use(cors());
 app.use(express.json());
 
+app.get('/test', (req, res) => res.send('Test route working'));
 
 app.get('/', (req, res) => {
     res.send('URL Shortener API is running');
 });
 
+
+app.use('/', urlRoutes); 
+
+
+// app.use(express.static('src/public'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-const connectDB = require('./config/index');
-connectDB();
-const urlRoutes = require('./routes/urlRoutes');
-app.use('/', urlRoutes);
-app.use(express.static('src/public'));
-
