02a4034c0036b8da135453ec4acda5645d240f85
Sparrow8104 <anchalsingh8102004@gmail.com>
2025-08-10 00:58:03 +0530

    Add package.json, lock file, source folder and update .gitignore

--- event-registration-system/src/server.js
+++ event-registration-system/src/server.js
@@ -12,6 +12,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const eventRoutes = require('./routes/eventRoutes');
+app.use('/events', eventRoutes);
+
+
 app.get('/', (req, res) => res.send('Event Registration System API running'));
 
 const PORT = process.env.PORT || 3000;
