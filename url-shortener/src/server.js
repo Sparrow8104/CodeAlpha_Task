66826aaefa71f106dc54e6eef3f3033a37819c4b
Sparrow8104 <anchalsingh8102004@gmail.com>
2025-08-09 22:58:26 +0530

    Add basic Express server for url-shortener

---
+++ url-shortener/src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+dotenv.config();
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('URL Shortener API is running');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
